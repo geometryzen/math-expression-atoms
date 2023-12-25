99c68efa366fba7c0f48bcd78ee42f8b908dd5a1
David Geo Holmes <geometryzen@gmail.com>
2023-12-25 16:36:57 -0500

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ export { Atom } from './atom/Atom';
 export { assert_boo, Boo, booF, booT, booU, create_boo, is_boo } from './boo/Boo';
 export { assert_flt, create_flt, Flt, is_flt } from './flt/Flt';
 export { Num } from './num/Num';
-export { BigInteger } from './rat/big-integer';
-export { assert_rat, create_int, create_rat, is_rat, Rat } from './rat/Rat';
+export { bigInt, BigInteger, gcd } from './rat/big-integer';
+export { assert_rat, create_int, create_rat, is_rat, negOne, one, Rat, zero } from './rat/Rat';
 export { assert_str, emptyStr, is_str, Str } from './str/Str';
 export { create_sym, create_sym_legacy, is_sym, Sym } from './sym/Sym';
+export { create_tensor_elements, create_tensor_elements_diagonal, create_tensor_elements_zero } from './tensor/create_tensor_elements';
 export { is_tensor, Tensor } from './tensor/Tensor';
 
 
