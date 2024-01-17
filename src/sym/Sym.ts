4062539baee4ac8d5e15ab30396aab77fda55625
David Geo Holmes <geometryzen@gmail.com>
2024-01-17 10:59:12 -0500

    Sym

--- src/sym/Sym.ts
+++ src/sym/Sym.ts
@@ -13,22 +13,35 @@ function strcmp(str1: string, str2: string): 0 | 1 | -1 {
     }
 }
 
-export function create_sym(printname: string, pos?: number, end?: number): Sym {
-    return new Sym(printname, pos, end);
+export function create_sym(localName: string, pos?: number, end?: number): Sym {
+    return create_sym_ns(localName, "", pos, end);
+}
+
+export function create_sym_ns(localName: string, namespace: string, pos?: number, end?: number): Sym {
+    return new Sym(localName, namespace, pos, end);
 }
 
 export class Sym extends Atom {
-    readonly #text: string;
+    readonly #localName: string;
+    readonly #namespace: string;
     /**
-     * Use create_sym to create a new Sym instance.
+     * Use create_sym_ns or create_sym to create a new Sym instance.
      */
-    constructor(text: string, readonly pos?: number, readonly end?: number) {
+    constructor(localName: string, namespace: string, readonly pos?: number, readonly end?: number) {
         super('Sym', pos, end);
-        this.#text = text;
+        this.#localName = localName;
+        this.#namespace = namespace;
     }
     compare(other: Sym): 1 | -1 | 0 {
-        // console.lg("compare", "this", this.ln, "other", other.ln);
-        return strcmp(this.#text, other.#text);
+        const x = strcmp(this.#namespace, other.#namespace);
+        switch (x) {
+            case 0: {
+                return strcmp(this.#localName, other.#localName);
+            }
+            default: {
+                return x;
+            }
+        }
     }
     contains(needle: U): boolean {
         if (needle instanceof Sym) {
@@ -38,12 +51,17 @@ export class Sym extends Atom {
     }
     /**
      * Creates a new symbol with exactly the same local name and namespace as this symbol.
-     * However it allows scanning information to be carried along with the new instance.
+     * However it allows scanning information (pos, end) to be carried along with the new instance.
      * @param pos The start position of the symbol in the source text.
      * @param end The end position of the symbol in the source text.
      */
-    clone(pos: number | undefined, end: number | undefined): Sym {
-        return create_sym(this.#text, pos, end);
+    clone(pos?: number | undefined, end?: number | undefined): Sym {
+        if (typeof pos === 'number' && typeof end === 'number') {
+            return create_sym_ns(this.#localName, this.#namespace, pos, end);
+        }
+        else {
+            return create_sym_ns(this.#localName, this.#namespace, this.pos, this.end);
+        }
     }
     override equals(other: U): boolean {
         if (other instanceof Sym) {
@@ -56,20 +74,40 @@ export class Sym extends Atom {
             return true;
         }
         else {
-            return this.#text === other.#text;
+            if (this.#localName === other.#localName) {
+                if (this.#namespace === other.#namespace) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
         }
     }
     key(): string {
-        return this.#text;
+        if (this.#namespace.length > 0) {
+            return `:${this.#namespace}/${this.#localName}`;
+        }
+        else {
+            return this.#localName;
+        }
     }
-    get printname(): string {
-        return this.#text;
+    get localName(): string {
+        return this.#localName;
     }
-    get text(): string {
-        return this.#text;
+    get namespace(): string {
+        return this.#namespace;
     }
     override toString(): string {
-        return this.#text;
+        if (this.#namespace.length > 0) {
+            return `${this.name}(${JSON.stringify(this.#localName)}, ${JSON.stringify(this.#namespace)})`;
+        }
+        else {
+            return `${this.name}(${JSON.stringify(this.#localName)})`;
+        }
     }
 }
 
